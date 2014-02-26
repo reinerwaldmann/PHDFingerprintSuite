3adfd852d007c0d830c31fe8d8e8f1d6b4dd3870
reinerwaldmann <mksd@list.ru>
2014-02-26 12:55:31 +0400

    FRRBuilder: first lines

--- PHDFRRBuilder/source/PHDFRRBuilder/main.cpp
+++ PHDFRRBuilder/source/PHDFRRBuilder/main.cpp
@@ -1,8 +1,68 @@
 #include <QCoreApplication>
+ #include <QTextStream>
+#include <QString>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    QTextStream  cout (stdout);
+    
+/*
+    for (int i=1; i<argc; i++)
+    {
+        cout<<argv[i];
+        cout<<"\n";
+    }
+
+    */
+
+
+    /*leftthumb.txt out.txt 0.9 0.0001*/
+    QString infile (argv [1]);
+    QString outfile (argv [2]);
+
+    double start = QString(argv[3]).toDouble();
+    double step = QString(argv[4]).toDouble();
+
+
+
+    cout<<infile;
+    cout <<"\n";
+    cout<<outfile;
+    cout <<"\n";
+    cout<<start;
+    cout <<"\n";
+    cout<<step;
+    cout <<"\n";
+
+
+
+
+    QList <double> listOfValues; //список значений, читаемый из файла
+    /*
+        Временное решение проблемы - потом, наверное, считывать придётся в табличный контейнер или нечто типа того
+    */
+
+
+    //QFile infile ()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    cout.flush();
     return a.exec();
 }
